d2a8054168073dc66f920b08cb32141cadb6ed37
Martijn Vegter <martijn@martijnvegter.com>
2018-09-12 20:02:03 +0200

    This doesn't really make things faster, but I tried
    Improved accuracy.

---
+++ test/stick.js
@@ -0,0 +1,11 @@
+const SteamController = require('./../');
+const controller = new SteamController();
+
+controller.stick.on('move', (event) => {
+    const json = JSON.stringify(event, null, 4);
+
+    console.log(json);
+    console.log();
+});
+
+controller.connect();
