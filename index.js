2376ba8479e059546d3bbda0f0c194293f1c7739
Martijn Vegter <mail@mvegter.me>
2018-09-13 20:37:25 +0200

    I'll explain this when I'm sober .. or revert it
    Added ESLint and improved core code.

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
-/*!
+/*
+ *!
  * node-steam-controller
  * Copyright(c) 2017 Wes Thom
  * MIT Licensed
  */
 
-'use strict';
-
-module.exports = require('./lib/controller')
+module.exports = require('./lib/controller');
--- test/stick.js
+++ test/stick.js
@@ -1,11 +1,12 @@
 const SteamController = require('./../');
+
 const controller = new SteamController();
 
 controller.stick.on('move', (event) => {
-    const json = JSON.stringify(event, null, 4);
+  const json = JSON.stringify(event, null, 4);
 
-    console.log(json);
-    console.log();
+  console.log(json);
+  console.log();
 });
 
 controller.connect();
