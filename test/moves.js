5c10a746cf077534c32cb036cbc33d045ffe7f06
Martijn Vegter <martijn@martijnvegter.com>
2018-09-22 17:13:47 +0200

    Now added delete for real
    Implemented graceful shutdown of the controller.

--- test/moves.js
+++ test/moves.js
@@ -1,6 +1,27 @@
-const SteamController = require('./../');
+const controller = require('./../');
 
-const controller = new SteamController();
+/**
+ * Can't use `controller.connect` inside of the `error` event,
+ * else emitting won't work.
+ */
+function connect() {
+  controller.connect();
+}
+
+controller.on('error', (err) => {
+  /**
+   * Error code 404 is only thrown if the Steam Controller is not found;
+   * Ignoring the error message because we know what it will be.
+   */
+  if (err.code !== 404) {
+    console.log(err);
+  }
+
+  /**
+   * Attempt re-connecting to the Steam Controller.
+   */
+  setTimeout(connect, 1000);
+});
 
 [
   'ltrigger',
@@ -20,4 +41,11 @@ const controller = new SteamController();
   });
 });
 
-controller.connect();
+/**
+ * Graceful shutdown of HID.
+ */
+process.on('SIGINT', () => {
+  controller.disconnect();
+});
+
+connect();
