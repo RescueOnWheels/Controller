f593da12e0cddfb37701dca86c5ade517d6b969a
Martijn Vegter <mail@mvegter.me>
2018-11-26 21:43:35 +0100

    That's all folks

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const isRoot = require('is-root');
 
 /* Require elevated permission */
-if (!isRoot() && process.platform !== "win32") {
+if (!isRoot() && process.platform !== 'win32') {
   // eslint-disable-next-line no-console
   console.error('You are not allowed to run this app with root permissions.');
   process.exit(1);
